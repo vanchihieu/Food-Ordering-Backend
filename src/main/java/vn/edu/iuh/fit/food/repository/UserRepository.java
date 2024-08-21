16d927c871414cca5aff6cf545acebcdb6c0b876
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-21 22:37:32 +0700

    UserRepository

---
+++ src/main/java/vn/edu/iuh/fit/food/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package vn.edu.iuh.fit.food.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import vn.edu.iuh.fit.food.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    @Query("SELECT u FROM User u Where u.status='PENDING'")
+    public List<User> getPenddingRestaurantOwners();
+
+    public User findByEmail(String username);
+
+}
